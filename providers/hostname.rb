c47b20a8a1420d595138ccfc1f4c0b59e41feecf
Joe Hohertz <jhohertz@gmail.com>
2015-08-26 02:27:11 -0400

    do network restart immediately as it is likely to interfere with other daemon restarts when delayed

--- providers/hostname.rb
+++ providers/hostname.rb
@@ -243,7 +243,7 @@ action :set do
     only_if { platform_family?('rhel') }
     only_if { node['platform_version'] < '7.0' }
     not_if { ::File.readlines('/etc/sysconfig/network').grep(/HOSTNAME=#{fqdn}/).any? }
-    notifies :restart, 'service[network]', :delayed
+    notifies :restart, 'service[network]', :immediately
   end
 
   ruby_block 'show hostnamectl' do
